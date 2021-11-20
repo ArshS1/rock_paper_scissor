8544cbfca0c97174fc79b535f1a9c5ed8ce5aebb
ArshS1 <90710790+ArshS1@users.noreply.github.com>
2021-11-20 12:00:48 -0500

    new button

--- main.py
+++ main.py
@@ -21,13 +21,15 @@ player.grid(row = 1, column = 0)
 computer = Label(mainWindow, image = rockSignComp, bg = "#808080")
 computer.grid(row = 1, column = 4)
 
-#scores
+#score of player
 scorePlayer = Label(mainWindow, text = 0, font = 150, bg = "#808080", fg = "#FFFFFF")
 scorePlayer.grid(row = 1, column = 1)
+
+#scores of computer
 scoreComputer = Label(mainWindow, text = 0, font = 150, bg = "#808080", fg = "#FFFFFF")
 scoreComputer.grid(row = 1, column = 3)
 
-#button
+#button for each option rock, paper, scissor
 buttonRock = Button(mainWindow, width = 20, height = 2, text = "Rock", bg = "#FFFFFF", command = lambda:newPicture("buttonRock"))
 buttonRock.grid(row = 2, column = 1)
 buttonPaper = Button(mainWindow, width = 20, height = 2, text = "Paper", bg = "#FFFFFF", command = lambda:newPicture("buttonPaper"))
@@ -35,38 +37,46 @@ buttonPaper.grid(row = 2, column = 2)
 buttonScissor = Button(mainWindow, width = 20, height = 2, text = "Scissor", bg = "#FFFFFF", command = lambda:newPicture("buttonScissor"))
 buttonScissor.grid(row = 2, column = 3)
 
-#indicator for side
+#button for exiting the function
+exitButton = Button(mainWindow, width = 15, height = 2, text = "Click to Exit", command = mainWindow.destroy, bg = "#808080")
+exitButton.grid(row = 3 , column = 2)
+
+
+
+#indicator for each players side
 userSide = Label(mainWindow, text = "PLAYER", font = 50, bg = "#808080")
 userSide.grid(row = 0, column = 0)
 compSide = Label(mainWindow, text = "COMPUTER", font = 50, bg = "#808080")
 compSide.grid(row = 0, column = 4)
 
-#messages
+#messages for window
 message = Label(mainWindow, font = 50, bg = "#808080", fg = "#FFFFFF")
 message.grid(row = 1, column = 2)
 
 def newMessage(msg):
     message["text"] = msg
 
+
+#score update for player
 def newScorePlayer():
     score = int(scorePlayer["text"])
     score += 1
     scorePlayer["text"] = str(score)
 
+#score update for computer
 def newScoreComp():
     score = int(scoreComputer["text"])
     score += 1
     scoreComputer["text"] = str(score)
-#update pics??
 
+
+#updating the pictures as button is clicked 
 options = ["rock", "paper", "scissor"]
 
 def newPicture(choice):
 
     choiceComp = options[random.randint(0,2)]
-    print(choice, choiceComp)
     winner(choice, choiceComp)
-
     if choiceComp == "rock":
         computer.configure(image = rockSignComp)
     elif choiceComp == "paper":
@@ -82,7 +92,7 @@ def newPicture(choice):
         player.configure(image = scissorSign)
 
 
-#winner
+#changes text on main window and checks for winner by comparing different scenarios
 def winner(player, computer):
 
     if player == "buttonRock" and computer == "rock":
